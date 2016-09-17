300b23672498d3b4733b2eb6885ee312cfe24a37
gavin <gzq0616@sina.com>
2016-09-17 20:37:51 +0800

    first

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -3,14 +3,15 @@ import ReactDOM,{findDOMNode} from 'react-dom'
 import '../css/confirm.css'
 import cn from 'classnames'
 
-class Confirm extends React.Component{
+class Confirm extends React.Component {
     static defaultProps = {
         animation: 'zoom',
         animationSpeed: 500,
         animationBounce: 1.2,
         autoClose: false,
         backgroundDismiss: false,
-        cancel: function () {},
+        cancel: function () {
+        },
         cancelButton: 'Close',
         cancelButtonClass: 'btn-default',
         cancelKeys: [27], // ESC key
@@ -18,90 +19,93 @@ class Confirm extends React.Component{
         closeIcon: null,
         closeIconClass: false,
         columnClass: 'col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1',
-        confirm: function () {},
+        confirm: function () {
+        },
         confirmButton: 'Okay',
         confirmButtonClass: 'btn-default',
         confirmKeys: [13], // ENTER key
         container: 'body',
         content: 'Are you sure to continue?',
-        contentLoaded: function () {},
+        contentLoaded: function () {
+        },
         icon: '',
         keyboardEnabled: false,
-        onAction: function () {},
-        onClose: function () {},
-        onOpen: function () {},
+        onAction: function () {
+        },
+        onClose: function () {
+        },
+        onOpen: function () {
+        },
         opacity: 0.2,
         rtl: false,
         theme: 'white',
         title: 'Hello',
-        watchInterval: 100,
+        watchInterval: 100
     };
-    constructor(props){
-    	super(props);
-    	this.state = {
-            boxClicked:false,
 
-            rconfirm_class:["rconfirm", "rconfirm-"+this.props.theme],
-            rconfirm_style:{},
-
-            rconfirmbg_class:["rconfirm-bg","seen"],
-            rconfirmbg_style:{},
-
-            rbox_class:["rconfirm-box"],
-            rbox_style:{},
-
-            content_style:{},
-            contentPane_style:{},
-            
-            
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxClicked: false,
+            rconfirm_class: ["rconfirm", "rconfirm-" + this.props.theme],
+            rconfirm_style: {},
+            rconfirmbg_class: ["rconfirm-bg", "seen"],
+            rconfirmbg_style: {},
+            rbox_class: ["rconfirm-box"],
+            rbox_style: {},
+            content_style: {},
+            contentPane_style: {}
         }
-    };
+    }
 
-    _unmount(){
+    _unmount() {
         const element = this.props.element;
         ReactDOM.unmountComponentAtNode(element);
         element.remove();
-    };
+    }
 
-    _cancel(){
-        setTimeout(()=>{
+    _cancel() {
+        setTimeout(()=> {
             this._unmount();
         })
-    };
-    _getCSS(speed, bounce){
-        return {'WebkitTransitionDuration': speed / 1000 + 's',
-                'transitionDuration': speed / 1000 + 's',
-                'WebkitTransitionTimingFunction': 'cubic-bezier(.36,1.1,.2, ' + bounce + ')',
-                'transitionTimingFunction': 'cubic-bezier(.36,1.1,.2, ' + bounce + ')'
-            }
-    };
+    }
 
-    componentWillMount(){
+    _getCSS(speed, bounce) {
+        return {
+            'WebkitTransitionDuration': speed / 1000 + 's',
+            'transitionDuration': speed / 1000 + 's',
+            'WebkitTransitionTimingFunction': 'cubic-bezier(.36,1.1,.2, ' + bounce + ')',
+            'transitionTimingFunction': 'cubic-bezier(.36,1.1,.2, ' + bounce + ')'
+        }
+    }
+
+    componentWillMount() {
         this.setState({
-            rconfirmbg_style:Object.assign({},
+            rconfirmbg_style: Object.assign({},
                 this.state.rconfirmbg_style,
-                {opacity:this.props.opacity},
-                this._getCSS(this.props.animationSpeed,1)
+                {opacity: this.props.opacity},
+                this._getCSS(this.props.animationSpeed, 1)
             )
         })
-    };
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setDialogCenter();
-    };
+    }
 
     setDialogCenter() {
+        let contentHeight, paneHeight, style;
         if (this.state.contentPane_style.display == 'none') {
-            var contentHeight = 0;
-            var paneHeight = 0;
+            contentHeight = 0;
+            paneHeight = 0;
         } else {
-            var contentHeight = this.refs.content.clientHeight;
-            var paneHeight = this.refs.contentPane.clientHeight;
+            contentHeight = this.refs.content.clientHeight;
+            paneHeight = this.refs.contentPane.clientHeight;
             if (paneHeight == 0)
                 paneHeight = contentHeight;
         }
-        var off = 100;
-        var w = this.refs.content.clientWidth;
+        let off = 100;
+        let w = this.refs.content.clientWidth;
 
         //var s = '-clip-path: inset(0px 0px '+contentHeight+'px 0px);' +
         //    'clip-path: inset(0px 0px '+contentHeight+'px 0px)';
@@ -111,67 +115,75 @@ class Confirm extends React.Component{
         var topMargin = (windowHeight - boxHeight) / 2;
         var minMargin = 100;
         if (boxHeight > (windowHeight - minMargin)) {
-            var style = {
+            style = {
                 marginTop: minMargin / 2,
                 marginBottom: minMargin / 2
-            }
+            };
             document.body.classList.add("rconfirm-noscroll");
         } else {
-            var style = {
+            style = {
                 marginTop: topMargin
-            }
+            };
             document.body.classList.remove("rconfirm-noscroll");
         }
 
-
         this.setState({
-            content_style:Object.assign({},this.state.content_style,{clip:'rect(0px ' + (off + w) + 'px ' + contentHeight + 'px -' + off + 'px)'}),
-            contentPane_style:Object.assign({},this.state.contentPane_style,{height:contentHeight}),
-            rbox_style:Object.assign({},this.state.rbox_style,style,this._getCSS(this.props.animationSpeed, this.props.animationBounce))
+            content_style: Object.assign({}, this.state.content_style, {clip: 'rect(0px ' + (off + w) + 'px ' + contentHeight + 'px -' + off + 'px)'}),
+            contentPane_style: Object.assign({}, this.state.contentPane_style, {height: contentHeight}),
+            rbox_style: Object.assign({}, this.state.rbox_style, style, this._getCSS(this.props.animationSpeed, this.props.animationBounce))
         })
-    };
+    }
 
-    scrollHandle(e){
+    scrollHandle(e) {
         e.stopPropagation();
-        if(this.props.backgroundDismiss){
+        if (this.props.backgroundDismiss) {
             this._cancel()
-        }else{
-            this.setState({rbox_class:[...this.state.rbox_class,"hilight"]})
-            setTimeout(()=>{
-                this.setState({rbox_class:this.state.rbox_class.filter((value)=>value!=="hilight")})
-            },800)
+        } else {
+            this.setState({rbox_class: [...this.state.rbox_class, "hilight"]})
+            setTimeout(()=> {
+                this.setState({rbox_class: this.state.rbox_class.filter((value)=>value !== "hilight")})
+            }, 800)
         }
-    };
+    }
 
-    boxHandle(e){
+    boxHandle(e) {
         e.stopPropagation();
-        this.setState({boxClicked:true});
-    };
+        this.setState({boxClicked: true});
+    }
 
-    render(){
-    	return (
+    render() {
+        return (
             <div ref="rconfirm" className={cn(this.state.rconfirm_class)} style={this.state.rconfirm_style}>
-                <div ref="rconfirmbg" className={cn(this.state.rconfirmbg_class)} style={this.state.rconfirmbg_style} />
+                <div ref="rconfirmbg" className={cn(this.state.rconfirmbg_class)} style={this.state.rconfirmbg_style}/>
 
                 <div className="rconfirm-scrollpane" onClick={this.scrollHandle.bind(this)}>
                     <div ref="container" className="container">
                         <div ref="row" className="row">
-                            <div ref="rconfirmboxcontainer" className={"rconfirm-box-container "+ this.props.columnClass}>
-                                <div ref="rbox" className={cn(this.state.rbox_class)} style={this.state.rbox_style} onClick={this.boxHandle.bind(this)}>
+                            <div ref="rconfirmboxcontainer"
+                                 className={"rconfirm-box-container "+ this.props.columnClass}>
+                                <div ref="rbox" className={cn(this.state.rbox_class)} style={this.state.rbox_style}
+                                     onClick={this.boxHandle.bind(this)}>
 
-                                    <div ref="closeIcon" className="closeIcon" style={{display:this.props.closeIconClass?'block':'none'}} onClick={this._cancel.bind(this)}>
-                                        {this.props.closeIconClass?<i className={this.props.closeIconClass} />:'&times;'}
+                                    <div ref="closeIcon" className="closeIcon"
+                                         style={{display:this.props.closeIconClass?'block':'none'}}
+                                         onClick={this._cancel.bind(this)}>
+                                        {this.props.closeIconClass ?
+                                            <i className={this.props.closeIconClass}/> : '&times;'}
                                     </div>
 
                                     <Title {...this.props}/>
 
-                                    <div ref="contentPane" className="content-pane" style={this.state.contentPane_style}>
-                                        <div ref="content" className="content" style={this.state.content_style}><input type="text" className="form-control"/></div>
+                                    <div ref="contentPane" className="content-pane"
+                                         style={this.state.contentPane_style}>
+                                        <div ref="content" className="content" style={this.state.content_style}><input
+                                            type="text" className="form-control"/></div>
                                     </div>
 
                                     <div ref="buttons" className="buttons">
-                                        <button type="button" className={"btn "+this.props.confirmButtonClass}>{this.props.confirmButton}</button>
-                                        <button type="button" className={"btn "+this.props.cancelButtonClass} onClick={this._cancel.bind(this)}>{this.props.cancelButton}</button>
+                                        <button type="button"
+                                                className={"btn "+this.props.confirmButtonClass}>{this.props.confirmButton}</button>
+                                        <button type="button" className={"btn "+this.props.cancelButtonClass}
+                                                onClick={this._cancel.bind(this)}>{this.props.cancelButton}</button>
                                     </div>
 
                                     <Rclear />
@@ -181,21 +193,22 @@ class Confirm extends React.Component{
                     </div>
                 </div>
             </div>
-    	)
+        )
     }
-};
+}
 
 
 //title
-class Title extends React.Component{
-    constructor(props){
+class Title extends React.Component {
+    constructor(props) {
         super(props)
     }
-    render(){
+
+    render() {
         return (
             <div className="title-c">
                 <span className="icon-c">
-                    <i className={this.props.icon} />
+                    <i className={this.props.icon}/>
                 </span>
                 <span className="title">{this.props.title}</span>
             </div>
@@ -205,18 +218,18 @@ class Title extends React.Component{
 
 
 //Rclear
-class Rclear extends React.Component{
-    render(){
+class Rclear extends React.Component {
+    render() {
         return (
-            <div className="r-clear" />
+            <div className="r-clear"/>
         )
     }
 }
 
-export default function confirm(options={}){
+export default function confirm(options = {}) {
     const element = document.createElement('div');
-	document.body.appendChild(element);
+    document.body.appendChild(element);
 
-    const props = Object.assign({element},options);
-	ReactDOM.render(<Confirm {...props}/>,element)
+    const props = Object.assign({element}, options);
+    ReactDOM.render(<Confirm {...props}/>, element)
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from './contains/App'
 
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('app')
+    <App />,
+    document.getElementById('app')
 );
